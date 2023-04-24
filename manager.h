920f3ef0529a0c918e51605df1cb28dc3a4f2558
minyoung10 <22000374@handong.ac.kr>
2023-04-24 16:47:48 +0900

    Add saveData method

--- manager.h
+++ manager.h
@@ -4,5 +4,5 @@ int selectMenu();
 void listProduct(Product *p,int count);
 int selectDataNo(Product *p, int count);
 
-void saveData(Product *p, int count);
+void saveData(Product p[], int count);
 int loadData(Product *p);
